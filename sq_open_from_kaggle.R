9f5aa4ef00f96b040c0f2a2a890b5de7ad98a94a
paACode <ackermann.pascal@gmail.com>
2025-03-05 11:04:42 +0100

    Fix, Download database first locally

--- sq_open_from_kaggle.R
+++ sq_open_from_kaggle.R
@@ -1,6 +1,18 @@
 
 require("DBI")
 require("RSQLITe")
+require("curl")
+url <- "https://www.kaggle.com/api/v1/datasets/download/hugomathien/soccer"
+
+# Destination file path to save the ZIP file
+zip_destfile <- "database.zip"
+
+# Download the ZIP file
+curl_download(url, zip_destfile)
+# Download the SQLite file
+curl_download(url, zip_destfile)
+
+unzip(zipfile = zip_destfile, "database.sqlite")
 
 #Connect to DB: https://www.kaggle.com/datasets/hugomathien/soccer
 con <- dbConnect(RSQLite::SQLite(), "database.sqlite")
