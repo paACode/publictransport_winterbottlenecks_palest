1d305813f638b03e1d43a38a1f2b18c4d8109cc1
sdfsdr <>
2025-03-20 14:32:45 +0100

    adding working file

---
+++ stefan_working_file.R
@@ -0,0 +1,2 @@
+data <- read.csv ("/Users/stefandreyfus/Library/CloudStorage/OneDrive-HochschuleLuzern/MSc/Vorlesungen/Semester_2/Applied Machine Learning and Predictive Modelling 1/Group Work my folder/publictransport_winterbottlenecks_palest/zentrahlbahn_nov_dataset_delay.csv")
+View(data)
