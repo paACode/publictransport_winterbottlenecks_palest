10525171d69bde5ca4ca5e6c36c0ec5223ab7391
sdfsdr <>
2025-03-21 17:16:52 +0100

    Changed EN -> DE

--- zb_code_day_time.R
+++ zb_code_day_time.R
@@ -18,13 +18,15 @@ zentralbahn_day_time <- zentralbahn_day_time %>%
   mutate(
     Hour_Ankunft = as.numeric(Hour_Ankunft),  # Convert Hour_Ankunft to numeric (if it's not already)
     day_time = case_when(
-      Hour_Ankunft >= 6  & Hour_Ankunft <= 10 ~ "morning",
-      Hour_Ankunft >= 11 & Hour_Ankunft <= 13 ~ "lunch",
-      Hour_Ankunft >= 14 & Hour_Ankunft <= 16 ~ "afternoon",
-      Hour_Ankunft >= 17 & Hour_Ankunft <= 19 ~ "evening",
-      (Hour_Ankunft >= 20 & Hour_Ankunft <= 23) | (Hour_Ankunft >= 0 & Hour_Ankunft <= 5) ~ "night",
+      Hour_Ankunft >= 6  & Hour_Ankunft <= 10 ~ "Vormittag",
+      Hour_Ankunft >= 11 & Hour_Ankunft <= 13 ~ "Mittag",
+      Hour_Ankunft >= 14 & Hour_Ankunft <= 16 ~ "Nachmittag",
+      Hour_Ankunft >= 17 & Hour_Ankunft <= 19 ~ "Abend",
+      (Hour_Ankunft >= 20 & Hour_Ankunft <= 23) | (Hour_Ankunft >= 0 & Hour_Ankunft <= 5) ~ "Nacht",
       TRUE ~ NA_character_  # Handles missing or unexpected values
     )
   )
 View (zentralbahn_day_time)
+
+write.csv(zentralbahn_day_time, "zentralbahn_day_time.csv", row.names = FALSE)
 
