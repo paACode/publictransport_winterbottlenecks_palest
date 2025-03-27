9cc0ab425af9d66fff5606cd3e5d7df6715dceab
sdfsdr <>
2025-03-27 13:44:19 +0100

    working a bit to have an overview

--- stefan_working_file.R
+++ stefan_working_file.R
@@ -1,2 +1,32 @@
-data <- read.csv ("/Users/stefandreyfus/Library/CloudStorage/OneDrive-HochschuleLuzern/MSc/Vorlesungen/Semester_2/Applied Machine Learning and Predictive Modelling 1/Group Work my folder/publictransport_winterbottlenecks_palest/zentrahlbahn_nov_dataset_delay.csv")
-View(data)
+#General codes
+zb_final <- read.csv("zentrahlbahn_final.csv", header = TRUE, stringsAsFactors = TRUE)
+
+str(zb_final)
+
+#First overview with plot
+
+plot(ANKUNFTDELAY_sec ~ TAGESZEIT, data = zb_final)
+"Similar spread, a lot of outlierts"
+
+plot(ANKUNFTDELAY_sec ~ RUSH_HOUR, data = zb_final)
+"Similar spread but narrower, a lot of outlierts"
+
+# Define colors for each category in TAGESZEIT
+color_map <- c("Abend" = "wheat4", 
+               "Vormittag" = "slateblue4", 
+               "Nachmittag" = "lightpink1", 
+               "Mittag" = "lightgreen", 
+               "Nacht" = "orangered2")
+
+"We see that there many delays in the morning at around 7.8C. In addition, it seems that there as well many delays in the night over all temperatures but the longest delays are at 0 and 6C."
+# Assign colors based on TAGESZEIT
+colors <- color_map[as.character(zb_final$TAGESZEIT)]
+
+# Plot with assigned colors
+plot(zb_final$w_temp_avg_c_Luzern, zb_final$ABFAHRTDELAY_sec, 
+     col = colors, pch = 19,
+     xlab = "Average Temperature (Luzern)", 
+     ylab = "Departure Delay (sec)")
+
+# Add a legend
+legend("topleft", pch = 19, legend = names(color_map), col = color_map)
