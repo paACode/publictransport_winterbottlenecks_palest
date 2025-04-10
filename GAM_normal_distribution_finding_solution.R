b568747c8024779873ee0b244269e5554f7751d0
sdfsdr <>
2025-04-10 14:55:36 +0200

    GAM model

--- GAM_normal_distribution_finding_solution.R
+++ GAM_normal_distribution_finding_solution.R
@@ -4,6 +4,7 @@
 library(dplyr)
 library(ggplot2)
 library(mgcv)
+library(tidyr)
 
 "In this project, we apply Generalized Additive Models (GAMs) to analyze the effects of various predictors on train delays, specifically focusing on the response variables ANKUNFTDELAY_min (arrival delay in minutes) and ABFAHRTDELAY_min (departure delay in minutes). 
 Due to the limited availability of continuous variables in our dataset these two delay metrics will be the response variables we will focus on.
@@ -16,10 +17,6 @@ This allows for a more reliable interpretation of the relationship between weath
 
 zb_final <- read.csv("zentrahlbahn_final.csv", header = TRUE, stringsAsFactors = TRUE)
 
-View(zb_final)
-
-str(zb_final)
-
 ## Subset the data
 
 # Define the specific Haltestellen
@@ -27,13 +24,10 @@ haltestellen_to_keep <- c("Luzern", "Luzern Allmend/Messe", "Kriens Mattenhof", 
                           "Hergiswil Matt", "Hergiswil NW", "Stansstad", "Stans")
 
 # Subset the data
-zb_final_subset <- zb_final %>%
-  filter(HALTESTELLEN_NAME %in% haltestellen_to_keep)
 
 zb_final_subset <- zb_final %>%
   filter(HALTESTELLEN_NAME %in% haltestellen_to_keep)
 
-str(zb_final_subset)
 
 #Removing rows NA in ANKUNFTDELAY_min and ABFAHRTDELAY_min
 
@@ -47,14 +41,12 @@ zb_final_subset <- zb_final_subset %>%
 
 sum(is.na(zb_final_subset$ABFAHRTDELAY_min)) #Checking if ABFAHRTDELAY_min NA is 0
 
-#Removing trains which have arrived or departured earlier
-zb_final_subset <- zb_final_subset %>%
-  filter(ANKUNFTDELAY_min >= 0, ABFAHRTDELAY_min >= 0)
-
 #Reducing the subbset just with the relevant columns
 
 zb_final_subset <- zb_final_subset %>% select(BETRIEBSTAG, LINIEN_TEXT, HALTESTELLEN_NAME, ANKUNFTSZEIT, AN_PROGNOSE, ABFAHRTSZEIT, AB_PROGNOSE, ABFAHRTDELAY_min, ANKUNFTDELAY_min, Delay_Category, TAGESZEIT, RUSH_HOUR, w_precip_mm_Luzern, w_temp_max_c_Luzern, STUNDE_ANKUNFT)
 
+
+str(zb_final_subset)
 
 View(zb_final_subset)
 
@@ -66,37 +58,37 @@ View(zb_final_subset)
 par(mfrow = c(1, 2))
 
 
-hist(zb_final$ANKUNFTDELAY_min, main = "Histogram of ANKUNFTDELAY_min", xlab = "ANKUNFTDELAY_min", col = "lightblue", breaks = 10, xlim = c(-2,8))
+hist(zb_final_subset$ANKUNFTDELAY_min, main = "Histogram of ANKUNFTDELAY_min", xlab = "ANKUNFTDELAY_min", col = "lightblue", breaks = 100, xlim = c(-2,8))
 
 
-hist(zb_final_subset$ABFAHRTDELAY_min, main = "Histogram of ABFAHRTDELAY_min", xlab = "ABFAHRTDELAY_min", col = "lightblue", xlim = c(-2,8))
+hist(zb_final_subset$ABFAHRTDELAY_min, main = "Histogram of ABFAHRTDELAY_min", xlab = "ABFAHRTDELAY_min", col = "lightblue", breaks = 100, xlim = c(-2,8))
 
 "We see that both response variables are not normally distrubuted. They seem to be right skewed. Lets check with the QQ plots. 
 "
 
 
 qqnorm(zb_final$ANKUNFTDELAY_min, main = "QQ Plot of ANKUNFTDELAY_min")
-qqline(zb_final$ANKUNFTDELAY_min, col = "red")
+qqline(zb_final$ANKUNFTDELAY_min, col = "lightblue")
 
 qqnorm(zb_final_subset$ABFAHRTDELAY_min, main = "QQ Plot of ABFAHRTDELAY_min")
-qqline(zb_final_subset$ABFAHRTDELAY_min, col = "red")
+qqline(zb_final_subset$ABFAHRTDELAY_min, col = "lightblue")
 
-"The QQ plots indicate that the response variables are right-skewed. Therefore, we apply a log transformation to improve the distribution and better meet model assumptions."
+"The QQ plots reveal that the response variables are right-skewed, which aligns with expectations. 
+In real-world train operations, early arrivals or departures are relatively uncommon compared to delays. 
+Moreover, the distribution indicates that longer delays occur less frequently than shorter ones, reflecting 
+typical delay patterns observed in Swiss public transportation. For this reason, we restrict our dataset for the 
+GAM analysis to observations with delays between -2 and +4 minutes, aiming to ensure a distribution that more closely 
+approximates the Gaussian assumption required by the model.
+"
 
-# Log transformation (handling 0 values with +1)
-zb_final_subset$log_ANKUNFTDELAY <- log(zb_final_subset$ANKUNFTDELAY_min + 1)
-zb_final_subset$log_ABFAHRTDELAY <- log(zb_final_subset$ABFAHRTDELAY_min + 1)
+zb_final_subset <- zb_final_subset %>%
+  filter(ABFAHRTDELAY_min >= -2 & ABFAHRTDELAY_min <= 4,
+         ANKUNFTDELAY_min >= -2 & ANKUNFTDELAY_min <= 4)
 
 
-# QQ Plot for log(ANKUNFTDELAY_min + 1)
-qqnorm(zb_final_subset$log_ANKUNFTDELAY,
-       main = "QQ Plot of log(ANKUNFTDELAY_min + 1)",
-       col = "darkblue", pch = 19)
-qqline(zb_final_subset$log_ANKUNFTDELAY, col = "red", lwd = 2)
+hist(zb_final_subset$ANKUNFTDELAY_min, main = "Histogram of ANKUNFTDELAY_min", xlab = "ANKUNFTDELAY_min", col = "lightgreen", breaks = 50, xlim = c(-2,4))
 
-# QQ Plot for log(ABFAHRTDELAY_min + 1)
-qqnorm(zb_final_subset$log_ABFAHRTDELAY,
-       main = "QQ Plot of log(ABFAHRTDELAY_min + 1)",
-       col = "darkblue", pch = 19)
-qqline(zb_final_subset$log_ABFAHRTDELAY, col = "red", lwd = 2)
+
+hist(zb_final_subset$ABFAHRTDELAY_min, main = "Histogram of ABFAHRTDELAY_min", xlab = "ABFAHRTDELAY_min", col = "lightgreen", breaks = 50, xlim = c(-2,4))
+
 
